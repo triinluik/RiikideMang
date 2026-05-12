f97afa34ed656d1f6091d39718e1572c8d62604c
triinluik <triin.malva@gmail.com>
2026-05-12 22:13:47 +0300

    Lisatud mängurežiimi valimine. Hetkel lippude äraarvamine puudub ja nupp viib pealinnade mängu.

---
+++ src/main/java/org/example/lippudemang/ManguReziim.java
@@ -0,0 +1,8 @@
+package org.example.lippudemang;
+
+// Võimalikud RiikideMängu režiimid
+public enum ManguReziim {
+    PEALINNAD,
+    LIPUD
+}
+
--- src/main/java/org/example/lippudemang/RiikideMangFX.java
+++ src/main/java/org/example/lippudemang/RiikideMangFX.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextInputDialog;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -13,16 +14,22 @@ import java.util.Optional;
 
 // Uus UI'ga peaklass
 public class RiikideMangFX extends Application {
+    private ManguReziim reziim;
 
     private Mang mang; //Mängu loogika objekt
     private Kusimus praeguneKusimus; // Hetkel kuvatav küsimus
+    private boolean kasVastatud; // Kontrollimaks, et ainult üks klaviatuuri või ekraanisündmus läheb kirja
+
     // UI komponendid
+    private Label juhendiLabel;
     private Label küsimusLabel;
     private Label punktidLabel;
     private Label tagasisideLabel;
+
     private Button[] vastuseNupud; //Nupud vastusevariantidega
     private Button uusMängNupp;
-    private boolean kasVastatud; // Kontrollimaks, et ainult üks klaviatuuri või ekraanisündmus läheb kirja
+    private Button režiimiValikNupp;
+
 
     @Override
     public void start(Stage lava) {
@@ -30,14 +37,21 @@ public class RiikideMangFX extends Application {
         mang = new Mang();
         kasVastatud = false; //Hetkel veel pole küsimusele vastatud
         // Loob tekstiväljad
+        juhendiLabel = new Label();
         küsimusLabel = new Label();
         punktidLabel = new Label("Punktid: 0");
-        tagasisideLabel = new Label("Vali õige vastus.");
+        tagasisideLabel = new Label();
         // loob massiivi nelja nupuga
         vastuseNupud = new Button[4];
+
         uusMängNupp = new Button("Alusta uuesti");
         uusMängNupp.setVisible(false);
         uusMängNupp.setOnAction(e -> alustaUutMängu());
+
+        režiimiValikNupp = new Button("Vali režiim");
+        režiimiValikNupp.setVisible(false);
+        režiimiValikNupp.setOnAction(e -> kuvaReziimiValik());
+
         // Paigutab nupud ruudustikuna
         GridPane nuppudePaneel = new GridPane();
         nuppudePaneel.setHgap(10);
@@ -57,16 +71,21 @@ public class RiikideMangFX extends Application {
         }
         // Paigutab elemendid vertikaalselt
         VBox juur = new VBox(15);
+        HBox mänguAlustamiseNupud = new HBox(10);
         juur.setStyle("-fx-padding: 20;"); //ääris
         // Lisab elemendid aknasse
         juur.getChildren().addAll(
                 new Label("Riikide mäng"),
-                new Label("Vali õige riik. Vastata saab hiirega või klahvidega 1–4."),
+                juhendiLabel,
                 küsimusLabel,
                 nuppudePaneel,
                 punktidLabel,
                 tagasisideLabel,
-                uusMängNupp
+                mänguAlustamiseNupud
+        );
+        mänguAlustamiseNupud.getChildren().addAll(
+                uusMängNupp,
+                režiimiValikNupp
         );
 
         Scene stseen = new Scene(juur, 500, 350);
@@ -81,11 +100,7 @@ public class RiikideMangFX extends Application {
 
                 // Kui vajutatakse muid numbreid
                 case DIGIT5, DIGIT6, DIGIT7,
-                     DIGIT8, DIGIT9, DIGIT0 ->
-
-                        tagasisideLabel.setText(
-                                "Palun vali number vahemikus 1–4."
-                        );
+                     DIGIT8, DIGIT9, DIGIT0 -> tagasisideLabel.setText("Palun vali number vahemikus 1–4.");
             }
         });
 
@@ -93,18 +108,88 @@ public class RiikideMangFX extends Application {
         lava.setScene(stseen);
         lava.show();
 
+        kuvaReziimiValik();
+    }
+
+    // Kuvab mängurežiimi valiku
+    private void kuvaReziimiValik() {
+
+        // Lähtestab mängu andmed
+        mang.alustaUuesti();
+        kasVastatud = false;
+        praeguneKusimus = null;
+
+        punktidLabel.setText("Punktid: 0");
+        tagasisideLabel.setText("");
+
+        küsimusLabel.setText("Vali mängurežiim:");
+        juhendiLabel.setText("Kas soovid arvata pealinnu või lippe?");
+
+        vastuseNupud[0].setText("1. Pealinnad");
+        vastuseNupud[1].setText("2. Lipud");
+
+        vastuseNupud[0].setDisable(false);
+        vastuseNupud[1].setDisable(false);
+
+        // Kuvab ainult kaks nuppu
+        vastuseNupud[0].setVisible(true);
+        vastuseNupud[1].setVisible(true);
+
+        // Peidab alumised nupud 2x2 gridis
+        vastuseNupud[2].setVisible(false);
+        vastuseNupud[3].setVisible(false);
+
+        // Käivitab pealinnade mängu
+        vastuseNupud[0].setOnAction(e -> alustaMäng(ManguReziim.PEALINNAD));
+
+        // Käivitab lippude mängu
+        vastuseNupud[1].setOnAction(e -> alustaMäng(ManguReziim.LIPUD));
+
+        // Peidab alumised juhtnupud
+        uusMängNupp.setVisible(false);
+        režiimiValikNupp.setVisible(false);
+    }
+
+    // Käivitab mängu valitud režiimis
+    private void alustaMäng(ManguReziim valitudReziim) {
+
+        // Kuvab kasutajale juhised
+        juhendiLabel.setText("Vastata saab hiirega või klahvidega 1-4.");
+        reziim = valitudReziim;
+
+        mang.alustaUuesti();
+        // Vastusenupud
+        for (int i = 0; i < vastuseNupud.length; i++) {
+
+            final int vastus = i + 1;
+
+            vastuseNupud[i].setVisible(true);
+            vastuseNupud[i].setDisable(false);
+
+            vastuseNupud[i].setOnAction(e -> kontrolliVastus(vastus));
+        }
+
+        punktidLabel.setText("Punktid: 0");
+        tagasisideLabel.setText("Vali õige vastus.");
+
         kuvaUusKüsimus();
     }
 
     // Alustab uue mängu
     private void alustaUutMängu() {
+        // Lähtestab mängu andmed
         mang.alustaUuesti();
         kasVastatud = false;
         punktidLabel.setText("Punktid: 0");
         tagasisideLabel.setText("Vali õige vastus.");
+
+        // Peidab alumised nupud
         uusMängNupp.setVisible(false);
         uusMängNupp.setDisable(false);
+        režiimiValikNupp.setVisible(false);
+        režiimiValikNupp.setDisable(false);
 
+        // Kuvab vastusenupud
         for (Button nupp : vastuseNupud) {
             nupp.setDisable(false);
             nupp.setVisible(true);
@@ -127,19 +212,25 @@ public class RiikideMangFX extends Application {
             // Näitab uue mängu nuppu
             uusMängNupp.setDisable(false);
             uusMängNupp.setVisible(true);
+            //Näitab režiimivaliku nuppu
+            režiimiValikNupp.setDisable(false);
+            režiimiValikNupp.setVisible(true);
             // Keelab nupud, sest rohkem pole neid vaja
             for (Button nupp : vastuseNupud) {
                 nupp.setVisible(false);
             }
             return;
         }
-        //Kuvad küsimuse teksti
-        küsimusLabel.setText(praeguneKusimus.getKüsimuseTekst());
+        //Kuvad küsimuse teksti olenevalt mängu režiimist
+        if (reziim == ManguReziim.PEALINNAD) {
+            küsimusLabel.setText(praeguneKusimus.getKüsimuseTekst());
+
+        } else if (reziim == ManguReziim.LIPUD) {
+            küsimusLabel.setText("Mis riigi lipuga on tegemist?");
+        }
         //Kuvad vastusevariandid nuppudel
         for (int i = 0; i < vastuseNupud.length; i++) {
-            vastuseNupud[i].setText(
-                    (i + 1) + ". "
-                            + praeguneKusimus.getVariandid().get(i).getRiigiNimi()
+            vastuseNupud[i].setText((i + 1) + ". " + praeguneKusimus.getVariandid().get(i).getRiigiNimi()
             );
         }
         // Uuendab punkte
@@ -164,7 +255,6 @@ public class RiikideMangFX extends Application {
             tagasisideLabel.setText(
                     "Vale! Õige vastus oli: "
                             + praeguneKusimus.getÕigePealinn().getRiigiNimi()
-                            + ". Punktid: " + mang.getPunktid()
             );
             for (Button nupp : vastuseNupud) {
                 nupp.setDisable(true);
@@ -173,6 +263,7 @@ public class RiikideMangFX extends Application {
             salvestaTulemus();
 
             uusMängNupp.setVisible(true);
+            režiimiValikNupp.setVisible(true);
         }
     }
 
